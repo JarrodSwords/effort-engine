c17e4cf9473b46294366ec649256372100d99d4b
Jarrod Swords <jarrod.swords@gmail.com>
2020-11-02 23:19:20 -0500

    Add projects

---
+++ ee-domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Effort.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ ee-test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Effort.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
