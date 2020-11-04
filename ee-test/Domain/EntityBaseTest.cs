11411adddb9b8ab665666c99465de184e9209f68
Jarrod Swords <jarrod.swords@gmail.com>
2020-11-04 15:44:58 -0500

    refactor

--- ee-test/Domain/EntityBaseTest.cs
+++ ee-test/Domain/EntityBaseTest.cs
@@ -10,11 +10,29 @@ namespace Effort.Test.Domain
         #region Test Methods
 
         protected abstract Entity CreateDifferentEntity(Guid id);
+
+        [Fact]
+        public void CreatedWithId_SetsId()
+        {
+            var id = Guid.NewGuid();
+            var entity = CreateEntity(id);
+
+            entity.Id.Value.Should().Be(id);
+        }
+
+        [Fact]
+        public void CreatedWithoutId_CreatesId()
+        {
+            var entity = CreateEntity();
+
+            entity.Id.Value.Should().NotBeEmpty();
+        }
+
         protected abstract Entity CreateEntity(Guid id);
         protected abstract Entity CreateEntity();
 
         [Fact]
-        public void EntitiesHaveIdentifierEquality()
+        public void HasIdentifierEquality()
         {
             var id = Guid.NewGuid();
             var entity1 = CreateEntity(id);
@@ -25,29 +43,12 @@ namespace Effort.Test.Domain
         }
 
         [Fact]
-        public void EntitiesHaveReferenceEquality()
+        public void HasReferenceEquality()
         {
             var entity1 = CreateEntity();
             var entity2 = entity1;
 
             entity2.Should().BeSameAs(entity1);
-        }
-
-        [Fact]
-        public void WhenCreatingEntity_WithId_EntityHasCorrectId()
-        {
-            var id = Guid.NewGuid();
-            var entity = CreateEntity(id);
-
-            entity.Id.Value.Should().Be(id);
-        }
-
-        [Fact]
-        public void WhenCreatingEntity_WithoutId_EntityHasId()
-        {
-            var entity = CreateEntity();
-
-            entity.Id.Value.Should().NotBeEmpty();
         }
 
         #endregion
