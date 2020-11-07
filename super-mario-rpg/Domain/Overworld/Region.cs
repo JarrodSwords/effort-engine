905d99ad59aa7ba11109d93a8c297038c48aa34c
Jarrod Swords <jarrod.swords@gmail.com>
2020-11-07 14:29:45 -0500

    add region

---
+++ super-mario-rpg/Domain/Overworld/Region.cs
@@ -0,0 +1,29 @@
+﻿using Effort.Domain;
+
+namespace SuperMarioRpg.Domain.Overworld
+{
+    public class Region : ValueObject<Region>
+    {
+        #region Core
+
+        public Region(string name)
+        {
+            Name = name;
+        }
+
+        #endregion
+
+        #region Public Interface
+
+        public string Name { get; }
+
+        #endregion
+
+        #region Equality, Operators
+
+        protected override bool EqualsExplicit(Region other) => Name == other.Name;
+        protected override int GetHashCodeExplicit() => Name.GetHashCode();
+
+        #endregion
+    }
+}
