8ebdfe5fec17c6865582330b20515eadae7057bf
Jarrod Swords <jarrod.swords@gmail.com>
2020-11-07 14:23:02 -0500

    Add location spec

---
+++ super-mario-rpg-test/Domain/Overworld/LocationSpec.cs
@@ -0,0 +1,15 @@
+﻿using Effort.Domain;
+using Effort.Test.Domain;
+using SuperMarioRpg.Domain.Overworld;
+
+namespace SuperMarioRpg.Test.Domain.Overworld
+{
+    public class LocationSpec : ValueObjectSpec<Location>
+    {
+        #region Protected Interface
+
+        protected override ValueObject<Location> CreateValueObject() => new Location("Mario's Pad");
+
+        #endregion
+    }
+}
