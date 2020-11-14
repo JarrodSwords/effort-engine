7013c64b89037a8baa78a7577ccbedb5d0f86267
Jarrod Swords <jarrod.swords@gmail.com>
2020-11-14 16:08:57 -0500

    Add def, hp, spatk, spdef, spd

--- super-mario-rpg-test/Domain/Battle/StatsSpec.cs
+++ super-mario-rpg-test/Domain/Battle/StatsSpec.cs
@@ -8,7 +8,7 @@ namespace SuperMarioRpg.Test.Domain.Battle
     {
         #region Protected Interface
 
-        protected override ValueObject<Stats> CreateValueObject() => new Stats(1);
+        protected override ValueObject<Stats> CreateValueObject() => new Stats(20, 0, 20, 10, 2, 20);
 
         #endregion
     }
--- super-mario-rpg/Domain/Battle/Stats.cs
+++ super-mario-rpg/Domain/Battle/Stats.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Effort.Domain;
+﻿using Effort.Domain;
 
 namespace SuperMarioRpg.Domain.Battle
 {
@@ -7,24 +6,41 @@ namespace SuperMarioRpg.Domain.Battle
     {
         #region Core
 
-        public Stats(ushort attack)
+        public Stats(byte attack, byte defense, byte hitPoints, byte specialAttack, byte specialDefense, byte speed)
         {
             Attack = attack;
+            Defense = defense;
+            HitPoints = hitPoints;
+            SpecialAttack = specialAttack;
+            SpecialDefense = specialDefense;
+            Speed = speed;
         }
 
         #endregion
 
         #region Public Interface
 
-        public ushort Attack { get; }
+        public byte Attack { get; }
+        public byte Defense { get; }
+        public byte HitPoints { get; }
+        public byte SpecialAttack { get; }
+        public byte SpecialDefense { get; }
+        public byte Speed { get; }
 
         #endregion
 
         #region Equality, Operators
 
-        protected override bool EqualsExplicit(Stats other) => Attack == other.Attack;
+        protected override bool EqualsExplicit(Stats other) =>
+            Attack == other.Attack
+         && Defense == other.Defense
+         && HitPoints == other.HitPoints
+         && SpecialAttack == other.SpecialAttack
+         && SpecialDefense == other.SpecialDefense
+         && Speed == other.Speed;
 
-        protected override int GetHashCodeExplicit() => Attack.GetHashCode();
+        protected override int GetHashCodeExplicit() =>
+            (Attack, Defense, HitPoints, SpecialAttack, SpecialDefense, Speed).GetHashCode();
 
         #endregion
     }
