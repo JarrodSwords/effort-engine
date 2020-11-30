8c8ba3e4ffe5289582f61942a2ab044428f0c589
Jarrod Swords <jarrod.swords@gmail.com>
2020-11-29 19:53:34 -0500

    Refactor views

---
+++ SuperMarioRpg.Wpf/Menus/Status/StatsView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace SuperMarioRpg.Wpf.Menus.Status
+{
+    /// <summary>
+    ///     Interaction logic for StatsView.xaml
+    /// </summary>
+    public partial class StatsView : UserControl
+    {
+        #region Creation
+
+        public StatsView()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
