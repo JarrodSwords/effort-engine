03a75625b672ad1a848a238d754147dc6393f5c7
Jarrod Swords <jarrod.swords@gmail.com>
2021-03-21 16:23:07 -0400

    add integration test

---
+++ super-mario-rpg-web-api-test-integration/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SuperMarioRpg.WebApi.Test.Integration
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
