856aeb0ffaa88a9e9e95b586687cbec412f235f7
Jarrod Swords <jarrod.swords@gmail.com>
2021-03-21 21:51:35 -0400

    add dispatcher

---
+++ ee/Domain/_cqrs/IDispatcher.cs
@@ -0,0 +1,12 @@
+﻿namespace Effort.Domain
+{
+    public interface IDispatcher
+    {
+        #region Public Interface
+
+        T Dispatch<T>(ICommand command);
+        T Dispatch<T>(IQuery<T> query);
+
+        #endregion
+    }
+}
---
+++ ee/Domain/_cqrs/ServiceProviderDispatcher.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Effort.Domain
+{
+    public class ServiceProviderDispatcher : IDispatcher
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        #region Creation
+
+        public ServiceProviderDispatcher(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        #endregion
+
+        #region IDispatcher Implementation
+
+        public T Dispatch<T>(ICommand command)
+        {
+            var type = typeof(ICommandHandler<>);
+            Type[] typeArgs = { command.GetType() };
+
+            var handlerType = type.MakeGenericType(typeArgs);
+            dynamic handler = _serviceProvider.GetService(handlerType);
+
+            var result = handler.Handle((dynamic) command);
+            return result;
+        }
+
+        public T Dispatch<T>(IQuery<T> query)
+        {
+            var type = typeof(IQueryHandler<,>);
+            Type[] typeArgs = { query.GetType(), typeof(T) };
+
+            var handlerType = type.MakeGenericType(typeArgs);
+            dynamic handler = _serviceProvider.GetService(handlerType);
+
+            var result = handler.Handle((dynamic) query);
+            return result;
+        }
+
+        #endregion
+    }
+}
