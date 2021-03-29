2db02f695dced46d78d2cea8e040e67afd00fb71
Jarrod Swords <jarrod.swords@gmail.com>
2021-03-28 23:57:42 -0400

    fix duplicate class

--- super-mario-rpg-application/character/CreateCharacter.cs
+++ super-mario-rpg-application/character/CreateCharacter.cs
@@ -44,24 +44,4 @@ namespace SuperMarioRpg.Application
 
         #endregion
     }
-
-    internal abstract class Handler<T> : ICommandHandler<T> where T : ICommand
-    {
-        protected readonly IUnitOfWork UnitOfWork;
-
-        #region Creation
-
-        protected Handler(IUnitOfWork unitOfWork)
-        {
-            UnitOfWork = unitOfWork;
-        }
-
-        #endregion
-
-        #region ICommandHandler<T> Implementation
-
-        public abstract void Handle(T command);
-
-        #endregion
-    }
 }
