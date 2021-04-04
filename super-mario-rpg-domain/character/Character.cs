9721b5099b07760bc36ca16e568e14c2e6f1b66f
Jarrod Swords <jarrod.swords@gmail.com>
2021-04-04 11:45:51 -0400

    character definition

---
+++ super-mario-rpg-domain/character/Character.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Effort.Domain;
+
+namespace SuperMarioRpg.Domain
+{
+    public abstract class Character : AggregateRoot
+    {
+        #region Creation
+
+        protected Character(Guid id, string name) : base(id)
+        {
+            Name = Name.CreateName(name);
+        }
+
+        #endregion
+
+        #region Public Interface
+
+        public Name Name { get; }
+
+        #endregion
+    }
+}
---
+++ super-mario-rpg-domain/character/NonPlayerCharacter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SuperMarioRpg.Domain
+{
+    public class NonPlayerCharacter : Character
+    {
+        #region Creation
+
+        public NonPlayerCharacter(string name, Guid id = default) : base(id, name)
+        {
+        }
+
+        #endregion
+    }
+}
