5b0376ec0cd600c3edfc67a976ae17d6ef10c3e5
Jarrod Swords <jarrod.swords@gmail.com>
2021-04-10 16:19:19 -0400

    cleanup

--- super-mario-rpg-domain/character/Enemy.cs
+++ super-mario-rpg-domain/character/Enemy.cs
@@ -25,15 +25,4 @@
 
         #endregion
     }
-
-    public record CombatStats(
-        ushort HitPoints,
-        short Attack,
-        short MagicAttack,
-        short Defense,
-        short MagicDefense,
-        decimal Evade,
-        decimal MagicEvade,
-        short Speed
-    );
 }
