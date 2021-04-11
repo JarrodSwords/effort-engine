a5b144277a061d5c3fafb2692cba6b396e2b5598
Jarrod Swords <jarrod.swords@gmail.com>
2021-04-11 15:31:36 -0400

    cleanup

--- effort-engine-domain/tiny-types/Id.cs
+++ effort-engine-domain/tiny-types/Id.cs
@@ -2,7 +2,5 @@ using System;
 
 namespace Effort.Domain
 {
-    public record Id(Guid Value) : TinyType<Guid>(Value == default ? Guid.NewGuid() : Value)
-    {
-    }
+    public record Id(Guid Value) : TinyType<Guid>(Value == default ? Guid.NewGuid() : Value);
 }
