424dd4b2f8579a42b5ec49aea3938d54527e7edb
Jarrod Swords <jarrod.swords@gmail.com>
2021-05-01 21:12:32 -0400

    project

---
+++ super-mario-rpg-seeder/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SuperMarioRpg.Seeder
+{
+    internal class Program
+    {
+        #region Static Interface
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        #endregion
+    }
+}
