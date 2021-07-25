775c92ac498d452af62f04f7031d38ff2a98106e
Jarrod Swords <jarrod.swords@gmail.com>
2021-07-25 17:52:08 -0400

    add equipment

---
+++ super-mario-rpg-domain/Combat/equipment/Equipment.cs
@@ -0,0 +1,15 @@
+﻿using Effort.Domain;
+
+namespace SuperMarioRpg.Domain.Combat
+{
+    public class Equipment : AggregateRoot
+    {
+        #region Creation
+
+        public Equipment(Id id) : base(id)
+        {
+        }
+
+        #endregion
+    }
+}
