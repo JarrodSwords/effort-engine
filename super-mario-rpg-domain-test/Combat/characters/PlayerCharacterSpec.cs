40f1b96d75e9003d4c56424a5cca703bd25e9084
Jarrod Swords <jarrod.swords@gmail.com>
2021-07-26 09:53:23 -0400

    create player character

---
+++ super-mario-rpg-domain-test/Combat/characters/PlayerCharacterSpec.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Effort.Domain;
+using Effort.Domain.Test;
+using SuperMarioRpg.Domain.Combat;
+
+namespace SuperMarioRpg.Domain.Test.Combat
+{
+    public class PlayerCharacterSpec : EntitySpec
+    {
+        #region Protected Interface
+
+        protected override Entity CreateEntity() => new PlayerCharacter();
+        protected override Entity CreateEntity(Guid id) => new PlayerCharacter(id);
+
+        #endregion
+    }
+}
---
+++ super-mario-rpg-domain/Combat/characters/player-character/PlayerCharacter.cs
@@ -0,0 +1,15 @@
+﻿using Effort.Domain;
+
+namespace SuperMarioRpg.Domain.Combat
+{
+    public class PlayerCharacter : AggregateRoot
+    {
+        #region Creation
+
+        public PlayerCharacter(Id id = default) : base(id)
+        {
+        }
+
+        #endregion
+    }
+}
